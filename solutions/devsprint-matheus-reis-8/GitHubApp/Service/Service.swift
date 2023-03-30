3eb0c2387e06e19d1efc6a9ae8455599002f0922
lucas vieira <lucasgomes114@gmai.com>
2023-03-30 15:29:58 -0300

    :sparkles: feat: add mocks on service

--- solutions/devsprint-matheus-reis-8/GitHubApp/Service/Service.swift
+++ solutions/devsprint-matheus-reis-8/GitHubApp/Service/Service.swift
@@ -8,9 +8,21 @@
 import Foundation
 
 struct Service {
-
-    func fetchList(_ completion: ([String]) -> Void) {
-
-        completion(["Repository 1", "Repository 2", "Repository 3"])
+    func fetchList(_ completion: ([Repository]) -> Void) {
+        completion([
+            Repository(name: "hereminders-ios", userName: "devpass-tech"),
+            Repository(name: "nexttunes-ios", userName: "devpass-tech"),
+            Repository(name: "challenge-onboarding", userName: "devpass-tech"),
+            Repository(name: "challeng-viewcode-realestate", userName: "devpass-tech"),
+            Repository(name: "challenge-mvvm-delivery", userName: "devpass-tech"),
+            Repository(name: "test-lucas", userName: "devpass-tech"),
+            Repository(name: "mock-121331141-124afas", userName: "devpass-tech"),
+            Repository(name: "mock--24fafs", userName: "devpass-tech"),
+            Repository(name: "mock-11313141-124fafs", userName: "devpass-tech"),
+            Repository(name: "mock-1213131141-24afafs", userName: "devpass-tech"),
+            Repository(name: "mock-1213111-2afafs", userName: "devpass-tech"),
+            Repository(name: "mock-12131114114aafs", userName: "devpass-tech"),
+            Repository(name: "mock-1213131141-1afafs", userName: "devpass-tech")
+        ])
     }
 }
