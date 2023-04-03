92ea97ef74d9431fbfada257a1a3a5338a0bfb0a
lucas vieira <lucasgomes114@gmai.com>
2023-04-03 16:25:31 -0300

    :sparkles: feat: add loading view

---
+++ solutions/devsprint-matheus-reis-8/GitHubApp/Models/LoadingViewConfiguration.swift
@@ -0,0 +1,12 @@
+//
+//  LoadingViewConfiguration.swift
+//  GitHubApp
+//
+//  Created by Lucas Gomes on 03/04/23.
+//
+
+import Foundation
+
+struct LoadingViewConfiguration {
+    let title: String
+}
